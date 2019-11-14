7b5a432b53523279727c5e68bc532754ddc1fbf9
Aaron Cooper <acooper@ceiamerica.com>
2019-11-14 14:58:12 -0500

    Change app style sheets to scss from css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   formGroup: FormGroup = new FormGroup(
